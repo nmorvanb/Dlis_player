15e016874e8c77926dd6f3bbe791458001f84a74
nmorvan <nicolas.morvan.benot@gmail.com>
2020-06-18 12:00:04 +0200

    javadoc + correction

--- ApiConnector.js
+++ ApiConnector.js
@@ -1,36 +1,3 @@
-const list = [
-    {
-       id: 1,
-       tracks:[{
-         titre: "House of The Rising Sun",
-         artiste: "The Animals",
-         duree: 320,
-         urlAudio: "",
-         urlVisuel: "",
-         urlPartage: "",
-         dureeEcoute: 1,
-         read: 1,
-         liked: 0,
-         validUntil: ""
-       }]
-   },
-   {
-      id: 2,
-      tracks:[{
-        titre: "Sultans of Swing",
-        artiste: "Dire Straits",
-        duree: 320,
-        urlAudio: "",
-        urlVisuel: "",
-        urlPartage: "",
-        dureeEcoute: 1,
-        read: 1,
-        liked: 0,
-        validUntil: ""
-      }]
-  }
-];
-
 class ApiConnector{
 
         
@@ -49,9 +16,11 @@ class ApiConnector{
      *Requette XTHML
      *Une requete a un etat
      *On veux donc savoir quand cet etat change, si il passe a 4 c'est la requete GET est appelée
-     *trackInfos recupere les donnes sous 
-     *xhr open effectue un POST sur la page ajax_function.php
-     *xhr send envoie a travers le POST, la varialble jsonString
+     *trackInfos recupere les donnes sous un format jsonString
+     *On utilise JSON.parse pour avoir un tableau associatif
+     *xhr open effectue un GET sur la page ajax_function.php
+     *on passe en parametre GET l'id de la musique à l'aide de l'url
+     *parametre f indique la fonction php de l'API à utiliser
     */
     getTrackInfos(idTrack){
       const xhr = new XMLHttpRequest();
@@ -59,10 +28,13 @@ class ApiConnector{
       xhr.onreadystatechange = function(){
         if (xhr.readyState == 4){
           if(xhr.status == 200){
-            var trackInfos = xhr.response;
-            var obj = JSON.parse(trackInfos);
-            console.log(obj);
-            return obj;
+            var trackInfos = JSON.parse(xhr.response);
+
+            //Console log qui montre qu'on recupere les trackinfos depuis ajax_function.php
+            console.log(trackInfos);
+            //Console log qu'on peut manipuler l'objet recupérer
+            console.log(trackInfos['artiste']);
+            return trackInfos;
           }
           else{
             if(xhr.status == 404){
@@ -73,7 +45,7 @@ class ApiConnector{
           }
         }
       
-      xhr.open("GET", "ajax_function.php?id="+idTrack,true);
+      xhr.open("GET", "ajax_function.php?id="+idTrack+"&f=trackjson",true);
       xhr.send();
  
        
@@ -92,7 +64,6 @@ class ApiConnector{
      *@param list
      *liste des informations envoyer par le post
      *function indique la fonction php à effectuer
-     *id_pers indique l'utilisateur
      *id_fichier indique la musique où l'on doit modifier la durée d'écoute
      *sec indique les secondes à ajouter a la durée
      *@param jsonString
@@ -105,7 +76,6 @@ class ApiConnector{
     setReadTime(idTrack, sec){
       var list = {
         function:"setReadTime",
-        id_pers : "10",
         id_fichier: idTrack,
         sec:sec
       }
@@ -116,7 +86,7 @@ class ApiConnector{
       xhr.open("POST", "ajax_function.php",true);
       xhr.setRequestHeader("Content-type", "application/json");
       xhr.send(jsonString);
-      //affiche dans la console le request payload
+      //affiche dans la console ce qui est envoyer en POST
       console.log(jsonString);
     }
   
@@ -129,7 +99,6 @@ class ApiConnector{
      *@param list
      *liste des informations envoyer par le post
      *function indique la fonction php à effectuer
-     *id_pers indique l'utilisateur
      *id_fichier indique la musique à like
      *@param jsonString
      *recupere la liste JSON en format String pour pouvoir l'envoyer dans le payloadRequest
@@ -142,17 +111,16 @@ class ApiConnector{
         
       var list = {
         function:"like",
-        id_pers : "10",
         id_fichier: idTrack,
       }
       const jsonString = JSON.stringify(list);
-      console.log(jsonString);
       const xhr = new XMLHttpRequest();
         
       xhr.open("POST", "ajax_function.php",true);
       xhr.setRequestHeader("Content-type", "application/json");
       xhr.send(jsonString);
-      
+      //affiche dans la console ce qui est envoyer en POST
+      console.log(jsonString);
   
     }
     /**
@@ -164,7 +132,6 @@ class ApiConnector{
      *@param list
      *liste des informations envoyer par le post
      *function indique la fonction php à effectuer
-     *id_pers indique l'utilisateur
      *id_fichier indique la musique à unlike
      *@param jsonString
      *recupere la liste JSON en format String pour pouvoir l'envoyer dans le payloadRequest
@@ -176,16 +143,16 @@ class ApiConnector{
     unlike(idTrack){
       var list = {
         function:"unlike",
-        id_pers : "10",
         id_fichier: idTrack,
       }
       const jsonString = JSON.stringify(list);
-      console.log(jsonString);
 
       const xhr = new XMLHttpRequest();
         
       xhr.open("POST", "ajax_function.php",true);
       xhr.setRequestHeader("Content-type", "application/json");
+      xhr.send(jsonString);
+      //affiche dans la console ce qui est envoyer en POST
       console.log(jsonString);
     }
   }
--- ajax_function.php
+++ ajax_function.php
@@ -3,25 +3,37 @@ require 'function.php';
 
 
 
-
+//Recupere le request payload
 $requestPayload = file_get_contents("php://input");
+//Si il existe c'est qu'une methode post à été appelée
 $object = json_decode($requestPayload,true);
+//L'object contient le nom de la fonction de l'api à appeler
+//on indique donc quelle fonction utilisée, et on utilise les reste des informations contenus dans l'objet
 if(isset($object)){
     if (isset($object['function'])) {
         if($object['function'] == "like"){
-            like($object['id_pers'],$object['id_fichier']);
+            like(10,$object['id_fichier']);
         }
         elseif($object['function'] == "unlike"){
-            unlike($object['id_pers'],$object['id_fichier']);
+            unlike(10,$object['id_fichier']);
         }
         elseif($object['function'] == "setReadTime"){
-            unlike($object['id_pers'],$object['id_fichier'],$object['sec']);
+            setReadTime(10,$object['id_fichier'],$object['sec']);
         }
     }
 }
+
 else{
-    if(isset($_GET["id"])){
-        echo trackjson($_GET["id"]);
+//à l'aide du parametre f on récupère la fonction de l'API qui doit être appelée
+    if(isset($_GET["f"])){
+        if($_GET["f"]=="trackjson"){
+//On doit récupérer les infos de la musique en fonction de l'id de celle ci
+//On vérifie donc qu'elle est existante et on appelle la fonction de l'api
+//le 2eme parametre est l'id session de l'utilisateur
+            if(isset($_GET["id"])){
+                echo trackjson($_GET["id"],10);
+            }
+        }
     }
 }
 
