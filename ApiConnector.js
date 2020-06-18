a5c0e2effd9ad69cecdb963ed8be8a69bd5fe574
nmorvan <nicolas.morvan.benot@gmail.com>
2020-06-18 10:28:23 +0200

    javadoc

--- ApiConnector.js
+++ ApiConnector.js
@@ -40,108 +40,153 @@ class ApiConnector{
     }
   
     /**
-    *Fonction like
-    *@param idTrack
-    *  l'id de la musique qui doit être like
-    * @param sec
-    * le nombre de seconde a ajouter au compteur d'écoute
-    *result recupere une liste des musiques ayant l'id demander à l'aide de filter
-    *comme il n'ya qu'une seule musique dans la liste, on va la récupérer en utilisant result[0].tracks[0]
-    *@return result[0].tracks[0]
-    *renvoie l'intégralité des infos d'une musique
+     *@function getTrackInfos()
+     *Cette fonction les information d'une musique
+     *Cette durée sera récupérer par l'API et ajouter au totale d'écoute dans la bdd
+     *@param idTrack
+     *l'id de la musique dont on modifie la duree d'écoute
+     *@param xhr
+     *Requette XTHML
+     *Une requete a un etat
+     *On veux donc savoir quand cet etat change, si il passe a 4 c'est la requete GET est appelée
+     *trackInfos recupere les donnes sous 
+     *xhr open effectue un POST sur la page ajax_function.php
+     *xhr send envoie a travers le POST, la varialble jsonString
     */
     getTrackInfos(idTrack){
-        var result = list.filter(function(element){
-          if (element.id == idTrack){
-            return true;
-          } else {
-            return false;
-          }
-        });
-    
-        
-        var track = {
-        titre: "House of The Rising Sun",
-        artiste: "The Animals",
-        duree: 320,
-        urlAudio: "",
-        urlVisuel: "",
-        urlPartage: "",
-        dureeEcoute: 1,
-        read: 1,
-        liked: 0,
-        validUntil: ""
+      const xhr = new XMLHttpRequest();
 
+      xhr.onreadystatechange = function(){
+        if (xhr.readyState == 4){
+          if(xhr.status == 200){
+            var trackInfos = xhr.response;
+            var obj = JSON.parse(trackInfos);
+            console.log(obj);
+            return obj;
+          }
+          else{
+            if(xhr.status == 404){
+              console.log("Ressource introuvable")
+            }
+          }
+
+          }
         }
-      }
+      
+      xhr.open("GET", "ajax_function.php?id="+idTrack,true);
+      xhr.send();
+ 
+       
+    }
     
   
     /**
-    *Fonction like
-    * @param idTrack
-    *  l'id de la musique qui doit être like
-    * @param sec
-    * le nombre de seconde a ajouter au compteur d'écoute
-    *result recurepere un²²e liste des musiques ayant l'id demander à l'aide de filter
-    *comme il n'ya qu'une seule musique dans la liste, on va la récupérer en utilisant result[0].tracks[0]
-    *On additionne le compteur existant aux secondes à ajouter
+     *@function setReadTime()
+     *Cette fonction envoie une durée d'écoute d'une musique par un utilisateur à l'aide d'un POST
+     *Cette durée sera récupérer par l'API et ajouter au totale d'écoute dans la bdd
+     *@param idTrack
+     *l'id de la musique dont on modifie la duree d'écoute
+     *@param sec
+     *le nombre de seconde a ajouter au compteur d'écoute
+     *Création d'une liste qui correspont aux données à envoyer dans le POST
+     *@param list
+     *liste des informations envoyer par le post
+     *function indique la fonction php à effectuer
+     *id_pers indique l'utilisateur
+     *id_fichier indique la musique où l'on doit modifier la durée d'écoute
+     *sec indique les secondes à ajouter a la durée
+     *@param jsonString
+     *recupere la liste JSON en format String pour pouvoir l'envoyer dans le payloadRequest
+     *@param xhr
+     *Requette XTHML
+     *xhr open effectue un POST sur la page ajax_function.php
+     *xhr send envoie a travers le POST, la varialble jsonString
     */
     setReadTime(idTrack, sec){
-        var result = list.filter(function(element){
-            if (element.id == idTrack){
-              return true;
-            } else {
-              return false;
-            }
-          });
-          result[0].tracks[0].dureeEcoute = result[0].tracks[0].dureeEcoute + sec;
+      var list = {
+        function:"setReadTime",
+        id_pers : "10",
+        id_fichier: idTrack,
+        sec:sec
+      }
+      const jsonString = JSON.stringify(list);
+
+      const xhr = new XMLHttpRequest();
+        
+      xhr.open("POST", "ajax_function.php",true);
+      xhr.setRequestHeader("Content-type", "application/json");
+      xhr.send(jsonString);
+      //affiche dans la console le request payload
+      console.log(jsonString);
     }
   
     /**
-    *Fonction like
-    *@param idTrack
-    *   l'id de la musique qui doit être like
-    *result recurepere une liste des musiques ayant l'id demander à l'aide de filter
-    *comme il n'ya qu'une seule musique dans la liste, on va la récupérer en utilisant result[0].tracks[0]
-    *On fixe l'attribue liked a 1 pour indiquer qu'il est like
+     *@function like()
+     *Cette fonction envoie une musique like par un utilisateur à l'aide d'un POST
+     *
+     *@param idTrack
+     *l'id de la musique qui doit être like
+     *@param list
+     *liste des informations envoyer par le post
+     *function indique la fonction php à effectuer
+     *id_pers indique l'utilisateur
+     *id_fichier indique la musique à like
+     *@param jsonString
+     *recupere la liste JSON en format String pour pouvoir l'envoyer dans le payloadRequest
+     *@param xhr
+     *Requette XTHML
+     *xhr open effectue un POST sur la page ajax_function.php
+     *xhr send envoie a travers le POST, la varialble jsonString
     */
     like(idTrack){
         
-      var result = {
-        function:"unlike",
+      var list = {
+        function:"like",
         id_pers : "10",
         id_fichier: idTrack,
       }
-      const jsonString = JSON.stringify(result);
-
+      const jsonString = JSON.stringify(list);
+      console.log(jsonString);
       const xhr = new XMLHttpRequest();
         
-      xhr.open("POST", "ajax_function.php");
+      xhr.open("POST", "ajax_function.php",true);
       xhr.setRequestHeader("Content-type", "application/json");
       xhr.send(jsonString);
+      
   
     }
     /**
-    *Fonction unlike
-    *@param idTrack
-    *   l'id de la musique qui doit être unlike
-    *result recurepere une liste des musiques ayant l'id demander à l'aide de filter
-    *comme il n'ya qu'une seule musique dans la liste, on va la récupérer en utilisant result[0].tracks[0]
-    *On fixe l'attribue liked a 0 pour indiquer qu'il est dislike
+     *@function unlike()
+     *Cette fonction envoie une musique unlike par un utilisateur à l'aide d'un POST
+     *
+     *@param idTrack
+     *l'id de la musique qui doit être unlike
+     *@param list
+     *liste des informations envoyer par le post
+     *function indique la fonction php à effectuer
+     *id_pers indique l'utilisateur
+     *id_fichier indique la musique à unlike
+     *@param jsonString
+     *recupere la liste JSON en format String pour pouvoir l'envoyer dans le payloadRequest
+     *@param xhr
+     *Requette XTHML
+     *xhr open effectue un POST sur la page ajax_function.php
+     *xhr send envoie a travers le POST, la varialble jsonString
     */
     unlike(idTrack){
-      var result = {
+      var list = {
         function:"unlike",
         id_pers : "10",
         id_fichier: idTrack,
       }
-      const jsonString = JSON.stringify(result);
+      const jsonString = JSON.stringify(list);
+      console.log(jsonString);
 
       const xhr = new XMLHttpRequest();
         
-      xhr.open("POST", "ajax_function.php");
+      xhr.open("POST", "ajax_function.php",true);
       xhr.setRequestHeader("Content-type", "application/json");
-      xhr.send(jsonString);
+      console.log(jsonString);
     }
   }
   
--- ajax_function.php
+++ ajax_function.php
@@ -1,13 +1,31 @@
 <?php
+require 'function.php';
+
+
+
+
 $requestPayload = file_get_contents("php://input");
 $object = json_decode($requestPayload,true);
-if (isset($object['liked'])) {
-  if($object['liked'] == 1){
-    like($object['id_pers'],$object['id_fichier']);
-  }
-  elseif($object['liked'] == 0){
-    unlike($object['id_pers'],$object['id_fichier']);
-  }
+if(isset($object)){
+    if (isset($object['function'])) {
+        if($object['function'] == "like"){
+            like($object['id_pers'],$object['id_fichier']);
+        }
+        elseif($object['function'] == "unlike"){
+            unlike($object['id_pers'],$object['id_fichier']);
+        }
+        elseif($object['function'] == "setReadTime"){
+            unlike($object['id_pers'],$object['id_fichier'],$object['sec']);
+        }
+    }
+}
+else{
+    if(isset($_GET["id"])){
+        echo trackjson($_GET["id"]);
+    }
 }
 
-var_dump($requestPayload);?>
+
+
+
+?>
